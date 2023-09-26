01086f094fdf737ea01aa16bc0abfe4e6a0060b8
siou.vteam <siou.vteam@gmail.com>
2023-09-26 15:49:00 +0800

    add datasets

---
+++ Index.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        self.cnn_layers = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),
+
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),
+
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(4, 4, 0),
+        )
+        self.fc_layers = nn.Sequential(
+            nn.Linear(256 * 8 * 8, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 11)
+        )
+
+    def forward(self, x):
+        # input (x): [batch_size, 3, 128, 128]
+        # output: [batch_size, 11]
+
+        # Extract features by convolutional layers.
+        x = self.cnn_layers(x)
+
+        # The extracted feature map must be flatten before going to fully-connected layers.
+        x = x.flatten(1)
+
+        # The features are transformed by fully-connected layers to obtain the final logits.
+        x = self.fc_layers(x)
+        return x
